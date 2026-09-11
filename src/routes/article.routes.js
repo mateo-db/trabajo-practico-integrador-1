7507376c9f76323e95b0f07af7d2b82d545b4488
mateo-db <cabezasluciano752@gmail.com>
2026-09-11 16:01:22 -0300

    feat: se montó rutas para modelo article

---
+++ src/routes/article.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { authentication } from "../middlewares/authMiddleware";
+import { createArticleValidations, getArticleByIdValidations, updateArticleValidations } from "../middlewares/validations/article.validations";
+import { checkValidationsResult } from "../middlewares/validationResult.middleware";
+import { createArticle, deleteArticleById, getAllPublishedArticles, getArticleById, getUserArticleById, getUserPublishedArticles, updateArticleById } from "../controllers/article.controller";
+import { areTheyOwner } from "../middlewares/ownerMiddleware";
+
+export const articleRoutes = Router()
+
+//Crear artículo. (usuario autenticado)
+articleRoutes.post('/articles', authentication, createArticleValidations, checkValidationsResult, createArticle)
+//Listar articulos publicados (usuario autenticado)
+articleRoutes.get('/articles', authentication, getAllPublishedArticles)
+//Obtener articulo por su id (usuario autenticado)
+articleRoutes.get('/articles/:id', authentication, getArticleByIdValidations, checkValidationsResult, getArticleById)
+//Listar articulos publicados del usuario logueado (usuario autenticado)
+articleRoutes.get('/articles/user', authentication, getUserPublishedArticles)
+//Obtener artículo del usuario logueado por su id.(usuario autenticado)
+articleRoutes.get('/articles/user/:id', authentication, getUserArticleById)
+//Actualizar artículo (solo autor o admin).
+articleRoutes.put('/articles/:id', authentication, areTheyOwner, updateArticleValidations, checkValidationsResult, updateArticleById)
+//Eliminar articulo (solo autor o admin)
+articleRoutes.delete('/articles/:id', authentication, areTheyOwner, deleteArticleById)
