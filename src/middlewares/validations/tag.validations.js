4eafa177128f465a27ca2b300c6f770bab3da189
mateo-db <cabezasluciano752@gmail.com>
2026-09-11 10:53:02 -0300

    fix: se modificó error de mensaje en middleware

--- src/middlewares/validations/tag.validations.js
+++ src/middlewares/validations/tag.validations.js
@@ -1,11 +1,21 @@
 import { body } from "express-validator";
 import { param } from "express-validator";
+import { Tag } from "../../models/tag.model.js";
 
 export const createTagValidations = [
     body("name")
     .notEmpty().withMessage("El nombre de la etiqueta es obligatorio")
     .isLength({ min: 2, max: 20 }).withMessage("El nombre de la etiqueta debe de tener entre 3 y 20 caracteres")
     .trim()
+    .custom(async (name) => {
+        const tagNameAlreadyExist = await Tag.findOne({
+            where: {name}
+        })
+        if (tagNameAlreadyExist) {
+            throw new Error("Ese nombre de etiqueta ya existe")
+        }
+        return true
+    })
 ]
 
 export const updateTagValidations = [
